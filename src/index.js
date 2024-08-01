fed42f94fa42aebb2e1c35cf56a4c1f86994e149
Alec Lang <lostlang@icloud.com>
2024-08-01 13:57:06 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+	switch (true) {
+		case n >= 0:
+			return Number(String(n).split("").reverse().join(""));
+		case n < 0:
+			return Number(String(-n).split("").reverse().join(""));
+	}
+};
